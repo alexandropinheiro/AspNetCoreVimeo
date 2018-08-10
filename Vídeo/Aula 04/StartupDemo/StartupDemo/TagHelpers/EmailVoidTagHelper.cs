1f2b606d7bdbe3655a9c97b781292d8eb933e5e5
alexandrorp_adm <alexandrorp_adm@CPU-7531>
2018-08-10 17:57:40 -0300

    Parte da View da aula 05

---
+++ Vídeo/Aula 04/StartupDemo/StartupDemo/TagHelpers/EmailVoidTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace StartupDemo.TagHelpers
+{
+    [HtmlTargetElement("email", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class EmailVoidTagHelper : TagHelper
+    {
+        public string MailTo { get; set; }
+        public string MailInfo { get; set; }
+        public override async Task ProcessAsync(TagHelperContent context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            output.Attributes.SetAttribute("href", "mailto:" + MailTo);
+            output.Content.SetContent(MailInfo);
+        }
+    }
+}

461df86888b5f9a3536f9359729311f70d8d3e80
alexandropinheiro <42052636+alexandropinheiro@users.noreply.github.com>
2018-08-11 00:55:10 -0300

    ViewComponent e TagHelper

---
+++ Vídeo/Aula 04/StartupDemo/StartupDemo/ViewComponent/CarrinhoViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StartupDemo.Models;
+
+namespace StartupDemo.ViewComponent
+{
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //LOGICA
+            var carrinho = new Carrinho()
+            {
+                TotalItens = 10,
+                ValorTotal = 550
+            };
+        }
+    }    
+}

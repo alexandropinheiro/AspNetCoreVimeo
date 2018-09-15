f9951ed269795ddadca18bf2068b281f316294bb
alexandropinheiro <42052636+alexandropinheiro@users.noreply.github.com>
2018-09-14 21:37:08 -0300

    Sincronizando arquivos

--- Video/AplicacaoCurso/Eventos.IO/src/Eventos.IO.InfraData/Repository/EventoRepository.cs
+++ Video/AplicacaoCurso/Eventos.IO/src/Eventos.IO.InfraData/Repository/EventoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Dapper;
 using Eventos.IO.Domain.Eventos;
 using Eventos.IO.Domain.Eventos.Repository;
 using Eventos.IO.InfraData.Context;
@@ -14,6 +15,16 @@ namespace Eventos.IO.InfraData.Repository
         {
 
         }
+
+        public override IEnumerable<Evento> ObterTodos()
+        {
+            var sql = "SELECT * FROM EVENTOS E " +
+                      " WHERE E.EXCLUIDO = 0 " +
+                      " ORDER BY E.DATAFIM DESC";
+
+            return Db.Database.GetDbConnection().Query<Evento>(sql);
+        }
+
         public void AdicionarEndereco(Endereco endereco)
         {
             Db.Enderecos.Add(endereco);
@@ -26,18 +37,43 @@ namespace Eventos.IO.InfraData.Repository
 
         public Endereco ObterEnderecoPorId(Guid id)
         {
-            return Db.Enderecos.Find(id);
+            var sql = @"SELECT * FROM ENDERECOS E " +
+                      " WHERE E.ID = @uid";
+
+            var endereco = Db.Database.GetDbConnection().Query<Endereco>(sql, new { uid = id });
+            return endereco.SingleOrDefault();
         }
 
         public IEnumerable<Evento> ObterEventoPorOrganizador(Guid organizadorId)
         {
-            return Db.Eventos.Where(e => e.OrganizadorId == organizadorId);
+            var sql = @"SELECT * FROM EVENTOS E " +                      
+                      " WHERE E.EXCLUIDO = 0 " +
+                      "   AND E.ORGANIZADORID = @oid " +
+                      " ORDER BY E.DATAFIM DESC";
+
+            var eventos = Db.Database.GetDbConnection().Query<Evento>(sql, new { oid = organizadorId });
+            return eventos;
         }
 
-        public override Evento ObterPorId(Guid Id)
+        public override Evento ObterPorId(Guid id)
         {
+            var sql = "SELECT * FROM EVENTOS E " +
+                      "  LEFT JOIN ENDERECOS EN " +
+                      "   ON E.ID = EN.EVENTOID " +
+                      " WHERE E.ID = @uid";
+
+            var evento = Db.Database.GetDbConnection().Query<Evento, Endereco, Evento>(sql,
+                (e, en) =>
+                {
+                    if (en != null)
+                        e.AtribuirEndereco(en);
+                    return e;
+                }, new { uid = id });
+
+            return evento.FirstOrDefault();
+
             //Isso é um Inner Join
-            return Db.Eventos.Include(e => e.Endereco).FirstOrDefault(e => e.Id == Id);
+            //return Db.Eventos.Include(e => e.Endereco).FirstOrDefault(e => e.Id == Id);
         }
     }
 }

e80f5e38d16d67f5ce981b3c2d459d71dd78a25d
alexandropinheiro <42052636+alexandropinheiro@users.noreply.github.com>
2018-09-26 09:14:31 -0300

    Aula 22 - Até o minuto 40:00

--- Video/FrontEnd/EventosFrontEnd/src/app/app.module.ts
+++ Video/FrontEnd/EventosFrontEnd/src/app/app.module.ts
@@ -2,24 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+// bootstrap
 import { CollapseModule } from 'ng2-bootstrap/collapse';
+import { CarouselModule } from 'ng2-bootstrap/carousel';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { MenuSuperiorComponent } from './shared/menu-superior/menu-superior.component';
 import { MainPrincipalComponent } from './shared/main-principal/main-principal.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuSuperiorComponent,
     MainPrincipalComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
